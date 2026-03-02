0c533309df76a5aa5d1f8880e4d382a70f046fd2
Elenxuan <yh670@exeter.ac.uk>
2026-03-02 15:11:46 +0000

    fixcode

--- cityrescue-starter/src/main/java/cityrescue/FireEngine.java
+++ cityrescue-starter/src/main/java/cityrescue/FireEngine.java
@@ -3,20 +3,20 @@ package cityrescue;
 import cityrescue.enums.IncidentType;
 import cityrescue.enums.UnitType;
 
-public class Ambulance extends Unit{
+public class FireEngine extends Unit{
     
-    public Ambulance(int sId, int uId, int x, int y)
+    public FireEngine(int sId, int uId, int x, int y)
     {
-        super(UnitType.AMBULANCE, sId, uId, x, y);
+        super(UnitType.FIRE_ENGINE, sId, uId, x, y);
     }
     @Override
     public boolean canHandle(IncidentType type)
     {
-        return type == IncidentType.MEDICAL;
+        return type == IncidentType.FIRE;
     }
     @Override
     public int getTicksToResolve(int severity)
     {
-        return 2;
+        return 4;
     }
 }
